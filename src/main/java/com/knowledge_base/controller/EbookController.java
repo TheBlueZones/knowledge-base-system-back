275259b4e5a2e76f65bcc1a8f3b62615615dbb9a
yide <3014453996@qq.com>
2022-10-13 11:57:18 +0800

    增加过滤器，打印接口耗时

---
+++ src/main/java/com/knowledge_base/controller/EbookController.java
@@ -0,0 +1,12 @@
+package com.knowledge_base.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+
+}
